52c4b36df8a06e71e4cefbbcf8c410d3187a7a41
ahmetbcakici <ahmetbcakici@gmail.com>
2020-05-23 10:50:28 +0300

    some minor improvements

--- routes/event.js
+++ routes/event.js
@@ -2,29 +2,33 @@
 import express from 'express';
 import Event from '../models/event';
 import User from '../models/user';
+import toSeoUrl from '../utils/toSeoUrl';
 
 const router = express.Router(); // call express.Router function to provide route
 
 router.get('/near', async (req, res) => {
   // need some validations
-  const x = await Event.find().sort({_id:-1}).limit(6);
-  console.log(x.reverse())
+  const x = await Event.find().sort({_id: -1}).limit(6);
+  console.log(x.reverse());
   //res.send(generatedEvent);
 });
 
-router.get('/:eventId', async (req, res) => {
+router.get('/:seoUrl', async (req, res) => {
   // event exist validation
-  const {eventId} = req.params;
-  const event = await Event.findById(eventId);
+  const {seoUrl} = req.params;
+  console.log(seoUrl);
+  const event = await Event.findOne({seoUrl});
+  if (!event) return res.status(404).send('Event not found!');
   const participantsDetails = await User.find({
-    username: {$in: event.participants},
+    _id: {$in: event.participants},
   });
-  event.participants = participantsDetails;
-  res.send(event);
+  res.json({event, participants:participantsDetails});
 });
 
 router.post('/generate', async (req, res) => {
+  const {title} = req.body;
   // need some validations
+  req.body.seoUrl = toSeoUrl(title);
   const generatedEvent = await Event.create(req.body);
   res.send(generatedEvent);
 });
